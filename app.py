ec995b80538942c6fe400768762f922f9233e77c
petcuiulyan <petcuiulyan@gmail.com>
2026-03-31 13:36:24 +0300

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-
+ 
 # Configurare pagină
 st.set_page_config(page_title="Aeroponic Pro Designer", layout="wide")
 
