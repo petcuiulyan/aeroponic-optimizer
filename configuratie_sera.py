d756ba8ccb040db8cb12d40b723c263f6e540bef
petcuiulyan <petcuiulyan@gmail.com>
2026-03-31 15:50:20 +0300

    Create configuratie_sera.py

---
+++ configuratie_sera.py
@@ -0,0 +1,14 @@
+# configuratie_sera.py
+def get_dimensiuni_sera(L, W, H, L_tech):
+    volum_total = L * W * H
+    suprafata_utila = (L - L_tech) * W
+    return {
+        "volum": volum_total,
+        "suprafata_utila": suprafata_utila,
+        "L_utila": L - L_tech
+    }
+
+def zona_germinare(L_tech, W):
+    # Estimăm capacitatea tăvilor de germinare în zona tehnică
+    nr_tavi = int((L_tech * W) / 0.5) # 0.5mp per tavă
+    return nr_tavi
