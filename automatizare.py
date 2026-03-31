32d0b1929907aebfa08581d0b82be8c636c045ed
petcuiulyan <petcuiulyan@gmail.com>
2026-03-31 16:02:10 +0300

    Update automatizare.py

--- automatizare.py
+++ automatizare.py
@@ -2,8 +2,9 @@
 def necesar_senzori(nr_turnuri):
     return {
         "esp32_count": 1, 
-        "senzori_ph_ec": 1, # În IBC1
-        "electrovalve": int(nr_turnuri / 2), # 1 per pereche
-        "metraj_cablu_date": nr_turnuri * 1.5,
-        "senzori_hala": 2 # Temp/Hum în extremități
+        "senzori_ph_ec": 1, 
+        "electrovalve_mari": 2, # Două valve principale (Sus/Jos)
+        "relee_putere": 2,      # Pentru valvele de debit mare
+        "metraj_cablu_date": nr_turnuri * 0.5, # Mai puțin cablu acum
+        "senzori_hala": 2 
     }
